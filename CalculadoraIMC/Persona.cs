95bdcbcbb648bb88d92bb35c3c2dd26604f88f8d
Bidkar Aragón Cárdenas <bidkar.aragon@uadeo.mx>
2024-04-18 15:54:52 -0700

    Definir la clase Persona

---
+++ CalculadoraIMC/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public float Estatura { get; set; }
+        public float Peso { get; set; }
+
+    }
+}
